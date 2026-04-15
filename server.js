b075ecf21bf5b314610a02960402a0d09669873e
rohit159962 <rthakur22270@outlook.com>
2026-04-16 00:48:14 +0530

    fix cors issue

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express()
 
 // Middleware
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://wandrai.vercel.app/"
-  ],
+  origin: true,
   credentials: true
 }));
 app.use(express.json())
